54ffb9f037060976feaa2a96b95bc3dc51fc2f06
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-08-08 16:18:29 +0800

    删除了原来的框架  创建了自己的框架

--- Assets/Scripts/Game/AppMain.cs
+++ Assets/Scripts/Game/AppMain.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppMain : MonoBehaviour
-{
-    void Start()
-    {
-        Debuger.EnableLog = true;
+public class AppMain : MonoBehaviour {
 
-        this.LogWarning("打豆豆");
-        this.LogError("睡觉");
-        this.Log(Debuger.LogFilePath);
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
